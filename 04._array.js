50cd82c192a47d315b04ec66bb9ed2396abd22eb
Prathameshgp48 <120002929+Prathameshgp48@users.noreply.github.com>
2025-05-24 11:11:49 +0000

    array basics and methods

---
+++ 04._array.js
@@ -0,0 +1,47 @@
+//Array
+const myArr = [0, 1, 2, 3, 4, 5]
+//shallow copy: object copy properties share the same reference of original one
+//deep copy: properties do not share the same references
+
+console.log(myArr[6])
+
+//another definition
+const myArr2 = new Array(1, 3, 4, 5)
+
+//array methods
+myArr.push(6)
+// myArr.push("2343")
+// console.log(myArr)
+// myArr.pop()
+// console.log(myArr)
+
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr)
+
+//questionier methods (true/false)
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3))  //-1 in case of unavailable value
+
+// const newArr = myArr.join()
+//join converts all arr into string
+// console.log(newArr, typeof newArr)
+
+//SLICE AND SPLICE
+console.log("A ", myArr)
+
+//slice take two input start(optional) index and end index(optional and exclusive) for slicing
+const myn1 = myArr.slice(1, 3)
+const myn3 = myArr.slice(1)
+//all operations on copy of original array
+//if start index not specified then slice consider 0 as start
+console.log(myn1)
+console.log(myn3)
+console.log("B ", myArr)
+
+//slice take two inputs: start index from where the elements be removed
+//second(optional and exclusive) is how many elements to remove
+const myn2 = myArr.splice(1, 3)//return deleted elements
+// const myn2 = myArr.splice(1) // remove all elements with and after specified start index
+console.log(myn2)
+console.log("c ", myArr)
---
+++ 05._array2.js
@@ -0,0 +1,28 @@
+const marvels = ["Ironman", "Spiderman", "Hulk"]
+const dcs = ["Superman", "Batman", "Robin"]
+
+// marvels.push(dcs)
+// console.log(marvels)
+
+//CORRECT METHODS TO MERGE ARRAYS
+// const new_heroes = marvels.concat(dcs)
+// console.log(new_heroes)
+
+// const all_heroes = [...marvels, ...dcs]
+// console.log(all_heroes)
+
+//ANOTHER METHODS
+// const another_arr = [2, 5, [4, 9, 1], 6, [2, 3, [4, 7]]]
+// //flat flatten nD array to 1D arr according to depth mentioned
+// const real_arr = another_arr.flat(Infinity)
+// console.log(real_arr)
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "APt"}))
+//it will return [] as we havent specified what(key or value) to consider to make arr
+
+const s1 = 100
+const s2 = 422
+const s3 = 324
+console.log(Array.of(s1, s2, s3))
